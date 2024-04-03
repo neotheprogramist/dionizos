4e669fdc1c7893cd58bc7c44fcef3ce920d479bc
Yberjon <nunnom1@gmail.com>
2024-04-03 16:07:48 +0200

    Add Estimate functionality

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -34,7 +34,7 @@ import {
   estimateAllContractsForAddress,
 } from "~/db/contract";
 import { getBtcUsdtPrice } from "~/db/price";
-//import { getUserBalanceByAddress } from "~/db/user";
+import { getUserBalanceByAddress } from "~/db/user";
 
 const QuoteSchema = z.object({
   quantity: z.number().nonnegative(),
@@ -90,7 +90,7 @@ export default component$(() => {
   const modalConfig = useContext(ModalConfigContext);
   const login = useContext(LoginContext);
   const userBalance = useSignal(0);
-  const userEstimatedBalance = useSignal(0);
+  const userEstimatedSettle = useSignal(0);
   const [quoteForm, { Form, Field }] = useForm<QuoteForm>({
     loader: useFormLoader(),
     action: useFormAction(),
@@ -139,7 +139,7 @@ export default component$(() => {
         <p class="text-gray-700">Address: {login.account.address}</p>
       )}
       <p class="text-lg font-semibold">Balance: ${userBalance.value}</p>
-      <p class="text-lg font-semibold">Estimated Balance: ${userEstimatedBalance.value}</p>
+      <p class="text-lg font-semibold">Estimated Settle: ${userEstimatedSettle.value}</p>
 
       <Form onSubmit$={handleSubmit} class="space-y-4">
         <Field name="quantity" type="number">
@@ -180,13 +180,13 @@ export default component$(() => {
           if (login.account && login.account.address) {
             const result = await estimateAllContractsForAddress(login.account.address);
             if (result) {
-              userEstimatedBalance.value = result.diff;
+              userEstimatedSettle.value = result.diff;
             }
           }
         }}
         class="mt-4 rounded bg-indigo-500 px-4 py-2 font-bold text-white hover:bg-indigo-700"
       >
-        Estimate Balance
+        Estimate Settle
       </button>
 
       <button
@@ -200,6 +200,7 @@ export default component$(() => {
             if (result && result.updateUserBalanceByAddressResult) {
               userBalance.value =
                 result.updateUserBalanceByAddressResult.balance;
+              userEstimatedSettle.value = 0
               console.log("diff", result.diff);
             }
           }
@@ -207,6 +208,22 @@ export default component$(() => {
         class="mt-4 rounded bg-yellow-500 px-4 py-2 font-bold text-white hover:bg-yellow-700"
       >
         Settle
+      </button>
+
+      <button
+        name="update-balance-button"
+        value="update-balance"
+        onClick$={async () => {
+          if (login.account && login.account.address) {
+            const result = await getUserBalanceByAddress(login.account.address);
+            if (result) {
+              userBalance.value = result;
+            }
+          }
+        }}
+        class="mt-4 rounded bg-indigo-500 px-4 py-2 font-bold text-white hover:bg-indigo-700"
+      >
+        Update Balance
       </button>
     </>
   );
